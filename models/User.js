0ff0aa6ec6aed06c9af0e354adea1815822e0df8
ebull2 <ethan.bull@live.co.uk>
2019-05-20 20:44:46 +0100

    7th commit

--- models/User.js
+++ models/User.js
@@ -17,6 +17,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+
     date: {
         type: Date,
         default: Date.now
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
+
+router.get('/', (req, res) => {
+    res.render('home');
+})
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard', {
+        firstName: req.user.firstName,
+        lastName: req.user.lastName
+    })
+})
+
+
+router.get('/logout', ensureAuthenticated, (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'Succesful Logout');
+    res.redirect('/account/signin');
+})
+
+
+
+router.get('/GAMEscreen', ensureAuthenticated, (req, res) => {
+  res.render('GAMEscreen');
+})
+
+router.get('/RANKscreen', ensureAuthenticated, (req, res) =>{
+  res.render('RANKscreen',{
+    firstName: req.user.firstName,
+    lastName: req.user.lastName
+
+  });
+})
+
+
+
+
+module.exports = router;
